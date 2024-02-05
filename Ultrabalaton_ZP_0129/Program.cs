efde597dccaf8db106203be12272ab65155b8c9a
Andevory <zaletnyik.peter@blathy.info>
2024-02-05 11:58:13 +0100

    Datastructure created and file read

--- Ultrabalaton_ZP_0129/Program.cs
+++ Ultrabalaton_ZP_0129/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] input = File.ReadAllLines("ub2017egyeni.txt");
+List<Verseny> data = new List<Verseny>();
+for (int i = 0; i < input.Length; i++)
+{
+    data.Add(new Verseny(input[i]));
+}
+
+struct Verseny
+{
+    public string Nev;
+    public string Rajtszam;
+    public string Kategoria;
+    public string Vido;
+    public int Tavszaz;
+
+    public Verseny(string line)
+    {
+        string[] splitted = line.Split(';');
+        this.Nev = splitted[0];
+        this.Rajtszam = splitted[1];
+        this.Kategoria = splitted[2];
+        this.Vido = splitted[3];
+        this.Tavszaz = int.Parse(splitted[4]);
+    }
+}
