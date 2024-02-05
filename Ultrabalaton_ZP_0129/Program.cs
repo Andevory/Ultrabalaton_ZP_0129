3b2a3644806665911956d0f9ca674c157e5ca3a2
Andevory <zaletnyik.peter@blathy.info>
2024-02-05 12:10:34 +0100

    Average men time in hours calculated

--- Ultrabalaton_ZP_0129/Program.cs
+++ Ultrabalaton_ZP_0129/Program.cs
@@ -1,6 +1,6 @@
 ﻿string[] input = File.ReadAllLines("ub2017egyeni.txt");
 List<Verseny> data = new List<Verseny>();
-for (int i = 0; i < input.Length; i++)
+for (int i = 1; i < input.Length; i++)
 {
     data.Add(new Verseny(input[i]));
 }
@@ -10,12 +10,24 @@ Console.WriteLine($"A versenyen {data.Count} résztvevő indult");
 int db = 0;
 for (int i = 0; i < data.Count; i++)
 {
-    if (data[i].Kategoria == "Női" && data[i].Tavszaz == 100)
+    if (data[i].Kategoria == "Noi" && data[i].Tavszaz == 100)
     {
         db++;
     }
 }
 Console.WriteLine($"A versenyt teljesítő női résztvevők száma: {db}");
+
+double osszido = 0;
+db = 0;
+for (int i = 0; i < data.Count; i++)
+{
+    if (data[i].Kategoria == "Ferfi" && data[i].Tavszaz == 100)
+    {
+        osszido += data[i].IdoOraban();
+        db++;
+    }
+}
+Console.WriteLine($"A teljes távot teljesítő férfi résztvevők átlag ideje: {osszido/db}");
 
 struct Verseny
 {
@@ -33,5 +45,14 @@ struct Verseny
         this.Kategoria = splitted[2];
         this.Vido = splitted[3];
         this.Tavszaz = int.Parse(splitted[4]);
+    }
+
+    public double IdoOraban()
+    {
+        string[] splitted = this.Vido.Split(":");
+        int vora = int.Parse(splitted[0]);
+        int vperc = int.Parse((splitted[1]));
+        int vmperc = int.Parse((splitted[2]));
+        return vora + (vperc/60) + (vmperc/3600);
     }
 }
